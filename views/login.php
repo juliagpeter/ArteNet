8b87cccc3f9b1341eaea7f3e540526eadb168138
Julia Peter <juliagpeter0@gmail.com>
2024-09-12 16:54:31 -0300

    background

--- views/login.php
+++ views/login.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../imagens/favicon.ico">
+    <link rel="stylesheet" href="../styles/listagens.css">
+    <script src="../scripts/script.js"></script>
     <title>Login</title>
-    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <main>
