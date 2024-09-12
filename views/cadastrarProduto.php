ecc46014d59cd9231c9bc54a2957ac2c1035b4fb
Julia Peter <juliagpeter0@gmail.com>
2024-09-12 16:19:14 -0300

    botao form

--- views/cadastrarProduto.php
+++ views/cadastrarProduto.php
@@ -20,7 +20,7 @@
 <body>
 <header>
     <nav class="navbar">
-        <button class="menu-toggle" onclick="toggleMenu()">☰ Menu</button>
+        <button class="menu-toggle">☰</button>
         <ul class="nav-list">
             <li><a href="index.php">Página Inicial</a></li>
             <li><a href="cadastrarPessoa.php">Adicionar Pessoa</a></li>
@@ -29,11 +29,9 @@
             <li><a href="cadastrarProduto.php">Adicionar Produto</a></li>
             <li><a href="listarProdutos.php">Listar Produto</a></li>
             <li><a href="gerenciarLances.php">Gerenciar Lances</a></li>
-            <li>
-                <form method="POST" action="../controller/PessoaController.php">
-                    <button type="submit" name="sair">Sair</button>
-                </form>
-            </li>
+            <form method="POST" action="../controller/PessoaController.php">
+                <button type="submit" name="sair">Sair</button>
+            </form>
         </ul>
     </nav>
 </header>
